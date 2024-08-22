0630a0b1b2a2addceae18755e25c27cde2afdc65
AliYagmurGames <ravenperception@gmail.com>
2024-08-22 16:24:54 +0300

    Description for the settings page

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,4 +1,6 @@
 <div class="wrap">
+    <!-- This is the page that shows up when you click on the plugin menu item directly
+        It includes some documentation and some settings -->
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
     <form method="post" action="options.php">
         <?php
