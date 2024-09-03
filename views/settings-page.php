bfee2a0770dc54000cfbc1b544a0f6e6e70e5f9c
Ali Yağmur <ravenperception@gmail.com>
2024-09-03 21:28:18 +0300

    Minor addition to settings page

--- views/settings-page.php
+++ views/settings-page.php
@@ -3,6 +3,14 @@
         It includes some documentation and some settings -->
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
+    <h2>Documentation</h2>
+
+    <p>This plugin is a simple real estate plugin that allows you to add properties to your website.</p>
+
+    <h2>Settings</h2>
+
+    <p>Create cities for the plugin:</p>
+    
     <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
         <?php wp_nonce_field('populate_cities_action_nonce', 'populate_cities_nonce'); ?>
         <input type="hidden" name="action" value="populate_cities_action">
