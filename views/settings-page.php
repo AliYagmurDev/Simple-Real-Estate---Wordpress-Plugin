5efa9bccbcb79ceb146325cbf49f1df4870d90c6
Ali Yağmur <ravenperception@gmail.com>
2024-09-06 20:51:57 +0300

    Settings page includes descripton for usable shortcodes

--- views/settings-page.php
+++ views/settings-page.php
@@ -7,6 +7,21 @@
 
     <p>This plugin is a simple real estate plugin that allows you to add properties to your website.</p>
 
+    <p>The shortcodes you can use are as follows:</p>
+
+    <ul>
+        <li>[property_price] - Displays the price of the property</li>
+        <li>[property_rooms] - Displays the number of rooms in the property</li>
+        <li>[property_address] - Displays the address of the property</li>
+        <li>[property_area] - Displays the area of the property</li>
+        <li>[property_floor] - Displays the floor of the property</li>
+        <li>[property_building_age] - Displays the building age of the property</li>
+        <li>[property_type] - Displays the type of the property</li>
+        <li>[property_city] - Displays the city of the property</li>
+        <li>[property_features] - Displays the features of the property</li>
+        <li>[ay_srer_search_form] - Displays a search form for the properties</li>
+    </ul>
+
     <h2>Settings</h2>
 
     <p>Create cities for the plugin:</p>
